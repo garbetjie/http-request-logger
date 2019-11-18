3a5ded6063d1adfde434d9cc93a70e0b80b30e42
Geoff Garbers <geoff@garbers.co.za>
2019-11-18 06:37:55 +0200

    Add ability to publish config file.

--- src/RequestLoggingServiceProvider.php
+++ src/RequestLoggingServiceProvider.php
@@ -8,12 +8,17 @@ use GuzzleHttp\HandlerStack;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Support\ServiceProvider;
 use function config;
+use function config_path;
 
 class RequestLoggingServiceProvider extends ServiceProvider
 {
     public function register()
     {
         $this->mergeConfigFrom(__DIR__ . '/../config.php', 'garbetjie-http-request-logging');
+
+        $this->publishes([
+            __DIR__ . '/../config.php' => config_path('garbetjie-http-request-logging.php'),
+        ]);
 
         $this->registerGuzzleClient();
         $this->registerGuzzleClientInterface();
