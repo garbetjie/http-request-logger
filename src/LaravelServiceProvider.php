83703387dc0b0c7702db1f7f0b23299001bac8d1
Geoff Garbers <geoff@garbers.co.za>
2021-07-20 10:08:02 +0200

    Add initial request & response event listening.

--- src/LaravelServiceProvider.php
+++ src/LaravelServiceProvider.php
@@ -7,16 +7,62 @@ use GuzzleHttp\Client;
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\HandlerStack;
 use Illuminate\Contracts\Container\Container;
+use Illuminate\Http\Client\Events\RequestSending;
+use Illuminate\Http\Client\Events\ResponseReceived;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+use SplObjectStorage;
 
 class LaravelServiceProvider extends ServiceProvider
 {
+	/**
+	 * Register classes for use.
+	 */
     public function register()
     {
         $this->registerGuzzleHandlerStackIfNotRegistered();
         $this->registerGuzzleClientIfNotRegistered();
         $this->registerGuzzleClientInterfaceIfNotRegistered();
     }
+
+	/**
+	 * Run at framework boot.
+	 *
+	 */
+    public function boot()
+	{
+		// If the event classes exist, then register the event listeners.
+		if (class_exists(RequestSending::class) && class_exists(ResponseReceived::class)) {
+			$this->bootHttpClientEvents();
+		}
+	}
+
+	/**
+	 * Listens for HTTP client events to log requests & responses.
+	 *
+	 */
+	protected function bootHttpClientEvents()
+	{
+		$logger = $this->app[Logger::class];
+		/* @var Logger $logger */
+
+		// Create a mapping between requests & request entries.
+		$requestEntryMap = new SplObjectStorage();
+
+		Event::listen(
+			RequestSending::class,
+			static function(RequestSending $event) use ($logger, $requestEntryMap) {
+				$requestEntryMap[$event->request] = $logger->request($event->request);
+			}
+		);
+
+		Event::listen(
+			ResponseReceived::class,
+			static function (ResponseReceived $event) use ($logger, $requestEntryMap) {
+				$logger->response($requestEntryMap[$event->request], $event->response);
+			}
+		);
+	}
 
     protected function registerGuzzleClientInterfaceIfNotRegistered()
     {
